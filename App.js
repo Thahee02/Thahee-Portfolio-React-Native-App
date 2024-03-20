f4dfe01a5d1038e65f99bd7aea6cdc4e037b26a9
HathilThahee <hathilthahee430@gmail.com>
2024-03-20 17:59:47 +0530

    built: Thahee portfolio app

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='light' />
+      <WebView
+      style={styles.container}
+      source={{ uri: 'https://thahee02.github.io/Thahee-Portfolio' }}
+      />
+    </>
   );
 }
 
@@ -16,5 +20,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
